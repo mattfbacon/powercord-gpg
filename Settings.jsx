5e14f1fd2eb941ef5b80a8ff02cebc00e5795078
Matt Fellenz <matt@felle.nz>
2022-02-11 22:16:14 -0800

    Make the settings page not crash Discord

--- Settings.jsx
+++ Settings.jsx
@@ -1,17 +1,12 @@
 const { React } = require('powercord/webpack');
 const { TextInput } = require('powercord/components/settings');
 
-module.exports = (props) => (
+module.exports = ({ getSetting, updateSetting }) => (
 	<div>
-		<TextInput onChange={(val) => this.props.updateSetting('gpg', val)} defaultValue={props.getSetting('gpg', 'gpg')} required={true} disabled={false} note="Name of the gpg executable on your system">
+		<TextInput onChange={(val) => updateSetting('gpg', val)} defaultValue={getSetting('gpg', 'gpg')} required={true} disabled={false} note="Name of the gpg executable on your system">
 			GPG Binary
 		</TextInput>
-		<TextInput
-			onChange={(val) => props.updateSetting('sender-fingerprint', val)}
-			defaultValue={props.getSetting('sender-fingerprint', '')}
-			required={true}
-			disabled={false}
-			note="Fingerprint of your personal public key (secret key must be available)">
+		<TextInput onChange={(val) => updateSetting('sender-fingerprint', val)} defaultValue={getSetting('sender-fingerprint', '')} required={true} disabled={false} note="Fingerprint of your personal public key (secret key must be available)">
 			Sender PGP Key
 		</TextInput>
 	</div>
